4657fd27f4ecfbf248c8c89b4a811f87eb76d23a
majitaki <k1830034@edu.cc.uec.ac.jp>
2019-06-20 16:51:06 +0100

    remove nineaxis_address.h

--- src/sensor/nineaxis_constant.h
+++ src/sensor/nineaxis_constant.h
@@ -1,7 +1,3 @@
 #pragma once
 
-const static double NINEAXIS_UPDATE_INTERVAL_TIME = 0.5;
-//const static double NINEAXIS_SAMPLE_INTERVAL_TIME = 0.05;
-const static int ACCEL_SAMPLES = 10;
-const static int GYRO_SAMPLES = 10;
-const static int MAGNET_SAMPLES = 10;
+const static double NINEAXIS_UPDATE_INTERVAL_TIME = 0.1;
