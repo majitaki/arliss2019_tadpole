4c3cc2f6f472618d2c1cdff6adf433de6b30b823
majitaki <k1830034@edu.cc.uec.ac.jp>
2019-06-30 19:32:52 +0900

    fix servo value setting

--- src/actuator/servo_constant.h
+++ src/actuator/servo_constant.h
@@ -9,19 +9,19 @@ const static int MIN_RAW_VALUE = 3500;
 
 //neck
 const static int NECK_ID = 2;
-const static int NECK_INNER = 7000;
-const static int NECK_CENTER = 7500;
-const static int NECK_OUTER = 9000;
+const static int NECK_INNER = 7600;
+const static int NECK_CENTER = 7600;
+const static int NECK_OUTER = 5700;
 //direct
 const static int DIRECT_ID = 0;
-const static int DIRECT_RIGHT = 6000;
+const static int DIRECT_RIGHT = 9000;
 const static int DIRECT_CENTER = 7500;
-const static int DIRECT_LEFT = 9000;
+const static int DIRECT_LEFT = 6000;
 //waist
 const static int WAIST_ID = 2;
-const static int WAIST_INNER = 9500;
+const static int WAIST_INNER = 10000;
 const static int WAIST_CENTER = 7500;
-const static int WAIST_OUTER = 5500;
+const static int WAIST_OUTER = 5000;
 //stabi
 const static int STABI_ID = 1;
 const static int STABI_INNER = 5000;
