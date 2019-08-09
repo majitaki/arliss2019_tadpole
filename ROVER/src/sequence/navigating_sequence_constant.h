22f6ee74f01dcbe9f6cb2f9f6712c220494181be
yotayota757 <nextgenerationofyota@gmail.com>
2019-08-09 15:22:17 +0900

    after kikyuu

--- ROVER/src/sequence/navigating_sequence_constant.h
+++ ROVER/src/sequence/navigating_sequence_constant.h
@@ -8,7 +8,7 @@ const static int NAVIGATING_STUCK_COUNT = 3;
 const static int NAVIGATING_NEAR_MODE_LIMIT = 3;
 const static double NAVIGATING_STUCK_CHECK_INTERVAL_TIME = 3; //second
 const static double NAVIGATING_FREEZE_TIME = 10;
-const static double NAVIGATING_TURN_SLOPE = 0.1;
+const static double NAVIGATING_TURN_SLOPE = 0.2;
 
 const static double NAVIGATING_DIRECTION_UPDATE_INTERVAL = 1;//�i�s������ύX����Ԋu(�b) 2016/08/31 5->1
 const static double NAVIGATING_MAX_DELTA_ANGLE = 90;//���̑���ŕ����]������ő�̊p�x
